e3550da51a021ef23fb8be66f9af9d4bcc77ef25
Artem Nemtsov <nemtsovartem11@gmail.com>
2026-04-23 16:31:28 +0300

    правки

--- mirea1.py
+++ mirea1.py
@@ -9,15 +9,16 @@ import json
 file_name ='expenses.json'
 
 def load_data():
-    if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
-        with open(file_name, 'r', encoding='utf-8') as f:
-             content = f.read()
-        if content.strip():
-            return json.loads(content)  
-        else:
-            return {'categories': [], 'expenses': []}
-        else:
+    if not os.path.exists(file_name):
         return {'categories': [], 'expenses': []}
+    try:
+        with open(file_name, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except Exception:
+        return {'categories': [], 'expenses': []}
+
 def save_data(data):
-     with open(file_name, 'w', encoding='utf-8')as f:
-            json.dump(data, f, ensure_ascii=False, indent=)
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    with open(file_name, 'w', encoding='utf-8')as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
