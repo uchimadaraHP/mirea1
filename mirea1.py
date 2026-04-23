49a38f55bf71310182bda4098d1d0cab7734fbd5
Artem Nemtsov <nemtsovartem11@gmail.com>
2026-04-23 16:03:12 +0300

    23.04.2026

--- mirea1.py
+++ mirea1.py
@@ -1,3 +1,8 @@
+#сделать структуру с ипсользованием json +
+#сделать лёгкий интерфейс +
+#написать фнукции, для красоты -
+#сделать всё модульно -
+
 import sys 
 import os
 import json
@@ -5,3 +10,9 @@ file_name ='expenses.json'
 
 def load_data():
     if not os.path.exists(file_name):
+        return {'categories': [], 'expenses': []}
+        try:
+            with open(file_name, 'r') as f:
+                    return json.load(f)
+        except Exception:
+             return {'categories': [], 'expenses': []}
